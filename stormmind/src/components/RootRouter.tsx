e8be199b1b17953e8507650ac229bb19b0fce93b
Nils Gämperli <n.gaemperli@gmail.com>
2025-05-15 17:44:20 +0200

    Implemented AppShell which allows for a responsive website

--- stormmind/src/components/RootRouter.tsx
+++ stormmind/src/components/RootRouter.tsx
@@ -1,11 +1,29 @@
+// src/components/RootLayout.tsx
+import { AppShell, Box } from '@mantine/core';
 import { Outlet } from 'react-router-dom';
-import { DoubleHeader } from '../components/Header/DoubleHeader';
+import { DoubleHeader } from './Header/DoubleHeader';
 
 export function RootLayout() {
   return (
-    <>
-      <DoubleHeader />
-      <Outlet /> {/* This renders the matched child route */}
-    </>
+    <AppShell
+      header={{ height: 84 }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <DoubleHeader />
+      </AppShell.Header>
+      <AppShell.Main>
+        <Box
+          style={{
+            height: 'calc(100vh - 84px)', // Subtract header height
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Outlet />
+        </Box>
+      </AppShell.Main>
+    </AppShell>
   );
 }
--- stormmind/src/pages/Home.tsx
+++ stormmind/src/pages/Home.tsx
@@ -1,19 +1,7 @@
 import DmgMap from '../components/DamageMap.tsx';
-import {Box} from'@mantine/core';
 
 export function Home() {
   return (
-    <>
-    <Box
-    h="calc(100vh - 84px)" 
-    display="flex"
-    >
-    <Box maw={400} w="100%" mx="auto" c="blue.6" bg="#fff">
-    
-        <DmgMap />
-    </Box>
-    </Box>
-
-    </>
+   <DmgMap/>
   );
 }
