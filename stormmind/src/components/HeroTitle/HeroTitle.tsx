d5e76e130171a9bce1ae6a9c614e3b64998b0199
duke-j <damian.x.ueltschi@protonmail.com>
2025-05-30 17:01:16 +0200

    pipelineTest

--- stormmind/src/components/HeroTitle/HeroTitle.tsx
+++ stormmind/src/components/HeroTitle/HeroTitle.tsx
@@ -11,7 +11,7 @@ export function HeroTitle() {
           <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'cyan' }} inherit>
             AI Innovation
           </Text>{' '}
-          for Storm Damage Forcasting.
+          for SSStorm Damage Forcasting.
         </h1>
 
         <Text className={classes.description} color="dimmed">
